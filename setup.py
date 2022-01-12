ed6866df48d43e78b1f3b4e51b9a5d8c7ab5f666
ejgenc@gmail.com <ejgenc@gmail.com>
2022-01-12 12:41:20 +0100

    Added 'setup.py' and 'environment.yml' to help with reproducibility. Also added .gitignore.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='1.0',
+    description=('Code for the "Scripting Languages" class final assignment'),
+    author='Eren Janberk Genç',
+    license='MIT',
+)
